87d6487b45f97534ad91da853c7c128dd26f9906
farrelarlistya <farrelpradipta77@gmail.com>
2026-03-11 23:46:07 +0700

    fix: correct application title from 'AgriSmar' to 'AgriSmart'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'AgriSmar',
+      title: 'AgriSmart',
       home: LoginPage(),
     );
   }
